4e8b2f7a0a17e9faa720d982e6965610bf85aa79
wadmin <wadmin@10.0.2.15>
2023-01-20 17:37:22 +0100

    A4.6 enunciado

---
+++ config/config.php
@@ -0,0 +1,30 @@
+<?php
+
+/* Default options */
+const DEFAULT_CONTROLLER = "Nota";
+const DEFAULT_ACTION = "list";
+
+const CONTROLLERS_FOLDER="controller";
+
+const MODELS_FOLDER = "model";
+
+const REPOSITORIES_FOLDER=MODELS_FOLDER.DIRECTORY_SEPARATOR."repository";
+const ENTITIES_FOLDER=MODELS_FOLDER.DIRECTORY_SEPARATOR."entity";
+const SERVICES_FOLDER=MODELS_FOLDER.DIRECTORY_SEPARATOR."service";
+
+
+
+const INCLUDES_FOLDER="includes";
+const TRAITS_FOLDER=MODELS_FOLDER.DIRECTORY_SEPARATOR."traits";
+
+const IMAGES_FOLDER="files";
+
+const IMAGE_DEFAULT="no-picture.jpg";
+
+
+
+const LOGIN_ERROR_MSG = "No se ha podido iniciar sesión";
+
+const ADMIN_ROLE="admin";
+const USER_ROLE="user";
+?>
